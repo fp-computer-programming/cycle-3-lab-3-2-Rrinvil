d6b3e7ac65edadad3fcc49fc5a7ad0046b3ec6bf
Rrinvil <144148135+Rrinvil@users.noreply.github.com>
2023-09-26 10:56:01 -0400

    Add files via upload

---
+++ lab_3-2.py
@@ -0,0 +1,31 @@
+'int(a)'
+"name error"
+"create statement and define a"
+
+
+
+"int('a')"
+" value error occured"
+"remove parenthesis so code isnt seen as comment"
+
+"'a' + 2" 
+"type error"
+"include a string and remove parentheis surrounding a"
+
+"import date"
+"module not found error"
+"create a module for 'date' "
+
+"print(Im a happy camper)"
+"syntax error"
+"include missing character which is the parenthesis"
+
+
+
+
+
+
+
+
+
+
